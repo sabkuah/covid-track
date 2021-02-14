99b464de9e239235eab7f0d318480dd9c8310676
Sabrina Kuah <swkuah@gmail.com>
2021-02-14 00:08:02 -0800

    add platform specific nav

--- App.js
+++ App.js
@@ -8,12 +8,17 @@ import DrawerNav from "./src/components/navigation/DrawerNav";
 
 const Stack = createStackNavigator();
 
+const PlatformSpecificNavigator = Platform.select({
+  ios: () => BottomTabNav,
+  android: () => DrawerNav,
+})();
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Root" component={DrawerNav} />
+          <Stack.Screen name="Root" component={PlatformSpecificNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
