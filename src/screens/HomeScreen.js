1204e52db9a6cbea621ed9cfeee1bb2506a13ff7
Sabrina Kuah <swkuah@gmail.com>
2021-02-13 23:50:42 -0800

    add bottom nav

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Text } from "react-native";
 
 const HomeScreen = () => {
   return (
