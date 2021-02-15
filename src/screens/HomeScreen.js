10854c04aeab7e4e6e05ff6d0b8c8382f46d02be
Sabrina Kuah <swkuah@gmail.com>
2021-02-15 13:09:13 -0800

    continent data moved to global tab

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,45 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import { Button } from "native-base";
-import covidAPI from "../api/covidAPI";
-import axios from "axios";
-import ContinentSummary from "../components/navigation/ContinentSummary";
 
 const HomeScreen = () => {
-  const [covidData, setCovidData] = useState([]);
-
-  const options = {
-    method: "GET",
-    url: "https://covid-193.p.rapidapi.com/statistics",
-    headers: {
-      "x-rapidapi-key": "35062213abmsh4851657f6e21028p101833jsn397fb1c7ac58",
-      "x-rapidapi-host": "covid-193.p.rapidapi.com",
-    },
-    parameters: ["Canada"],
-  };
-
-  const getCovidData = () => {
-    axios
-      .request(options)
-      .then(function (response) {
-        setCovidData(response.data.response);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
-
-  const filterByContinent = (continent) => {
-    return covidData.filter((data) => {
-      return data.continent === continent;
-    });
-  };
-
-  useEffect(() => {
-    getCovidData();
-    //console.log(covidData);
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.heading}>COVID LENS</Text>
@@ -52,25 +15,6 @@ const HomeScreen = () => {
           <Text>REGISTER</Text>
         </Button>
       </View>
-      <ContinentSummary title="Africa" results={filterByContinent("Africa")} />
-      <ContinentSummary
-        title="Antarctica"
-        results={filterByContinent("Antarctica")}
-      />
-      <ContinentSummary title="Asia" results={filterByContinent("Asia")} />
-      <ContinentSummary title="Europe" results={filterByContinent("Europe")} />
-      <ContinentSummary
-        title="North America"
-        results={filterByContinent("North-America")}
-      />
-      <ContinentSummary
-        title="Oceania"
-        results={filterByContinent("Oceania")}
-      />
-      <ContinentSummary
-        title="South America"
-        results={filterByContinent("South-America")}
-      />
     </SafeAreaView>
   );
 };
@@ -78,8 +22,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    //alignItems: "center",
-    //justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
   heading: {
     fontWeight: "400",
