a5280b546e06be75ecfb004f162aba9d3540389a
Sabrina Kuah <swkuah@gmail.com>
2021-02-19 14:58:07 -0800

    add form and onPress to Login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { StyleSheet, View, Text, SafeAreaView } from "react-native";
-import { Button } from "native-base";
+import { Container, Content, Form, Item, Input, Button } from "native-base";
 import base from "../styles/styles";
 
 const LoginScreen = () => {
+  const onLoginPressed = (e) => {
+    e.preventDefault();
+    console.log("login pressed");
+  };
+
+  const onRegisterPressed = (e) => {
+    console.log("register pressed");
+  };
   return (
     <SafeAreaView style={base.container}>
       <View style={styles.auth}>
-        <Button success style={styles.button}>
+        <Form style={styles.form}>
+          <Item>
+            <Input placeholder="Email" id="email" />
+          </Item>
+          <Item last>
+            <Input
+              placeholder="Password"
+              id="password"
+              secureTextEntry={true}
+            />
+          </Item>
+        </Form>
+
+        <Button success style={styles.button} onPress={onLoginPressed}>
           <Text style={base.textBold}>LOGIN</Text>
         </Button>
-        <Button success style={styles.button}>
+        <Button success style={styles.button} onPress={onRegisterPressed}>
           <Text style={base.textBold}>REGISTER</Text>
         </Button>
       </View>
@@ -23,6 +44,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width: "100%",
     flex: 1,
+  },
+  form: {
+    backgroundColor: "white",
+    width: "75%",
+    marginBottom: "5%",
   },
   button: {
     alignSelf: "center",
