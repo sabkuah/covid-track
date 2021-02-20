c7f8129604e2e9ef09846b6fb52d64b90bd834e4
Sabrina Kuah <swkuah@gmail.com>
2021-02-19 17:09:05 -0800

    firebase auth added

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,34 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import { Container, Content, Form, Item, Input, Button } from "native-base";
 import base from "../styles/styles";
+import firebase from "firebase";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
   const onLoginPressed = (e) => {
     e.preventDefault();
-    console.log("login pressed");
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        setUser(userCredential.user);
+        setPassword("");
+        // navigate
+        navigation.navigate("UserDashboardScreen", user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+
+    console.log("email>", email);
+    console.log("passowrd>", password);
   };
 
-  const onRegisterPressed = (e) => {
-    console.log("register pressed");
+  const onRegisterPressed = async (e) => {
+    console.log("password before>", password);
+
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        setUser(userCredential.user);
+        setPassword("");
+        console.log("user>>", user);
+        console.log("password after>", password);
+        // navigate
+        navigation.navigate("UserDashboardScreen", user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("Error>>", errorMessage);
+      });
   };
   return (
     <SafeAreaView style={base.container}>
       <View style={styles.auth}>
         <Form style={styles.form}>
           <Item>
-            <Input placeholder="Email" id="email" />
+            <Input
+              placeholder="Email"
+              value={email}
+              autoCompleteType="email"
+              autoCapitalize="none"
+              onChangeText={(text) => {
+                setEmail(text);
+              }}
+            />
           </Item>
           <Item last>
             <Input
               placeholder="Password"
-              id="password"
+              value={password}
               secureTextEntry={true}
+              onChangeText={(text) => {
+                setPassword(text);
+              }}
             />
           </Item>
         </Form>
 
-        <Button success style={styles.button} onPress={onLoginPressed}>
+        <Button
+          success
+          style={styles.button}
+          type="submit"
+          onPress={onLoginPressed}
+        >
           <Text style={base.textBold}>LOGIN</Text>
         </Button>
         <Button success style={styles.button} onPress={onRegisterPressed}>
--- src/screens/UserDashboardScreen.js
+++ src/screens/UserDashboardScreen.js
@@ -2,11 +2,12 @@ import React from "react";
 import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import base from "../styles/styles";
 
-const UserDashboardScreen = () => {
+const UserDashboardScreen = ({ route }) => {
+  const user = route.params;
   return (
     <SafeAreaView style={base.container}>
       <View style={styles.auth}>
-        <Text style={base.textBold}>you logged in!!!</Text>
+        <Text style={base.textBold}>you logged in!!! {user.email}</Text>
       </View>
     </SafeAreaView>
   );
